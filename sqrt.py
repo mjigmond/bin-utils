18e7ffe9c01f38718d84e3981af165e91554a1f3
Marius Jigmond <mjigmond@gmail.com>
2020-09-24 19:57:21 -0600

    whoops return type

--- sqrt.py
+++ sqrt.py
@@ -22,26 +22,26 @@ def user_sqrt(num: float, decimals: int) -> str:
 
     """
     if num <= 0:
-        return 0
-    
+        return '0'
+
     e = 3
-    
+
     fmtd_num = f'{num:.{decimals*2+e}f}'[:-e]
-    
+
     left, right = fmtd_num.split('.')
-    
+
     left = left[-1::-1]
     left_list = [left[i:i+2][-1::-1] for i in range(0, len(left), 2)]
     left_list.reverse()
-    
+
     right_list = [right[i:i+2] for i in range(0, len(right), 2)]
-    
+
     g = 1
     n = int(left_list[0])
     while g**2 <= n:
         g += 1
     lo = n - (g - 1)**2
-    
+
     res = str(g - 1)
     for n in left_list[1:] + right_list:
         lo = int(str(lo) + n)
@@ -51,19 +51,19 @@ def user_sqrt(num: float, decimals: int) -> str:
             i += 1
         lo = lo - (t + i - 1) * max(0, i - 1)
         res += str(max(0, i - 1))
-        
+
     sqrt = res[:len(left_list)] + '.' + res[len(left_list):]
     return sqrt
-    
+
 
 if __name__ == '__main__':
     for i in range(100):
         num = random.uniform(0, 1e7)
         dec = random.randint(0, 9)
-        
+
         math_sqrt = math.sqrt(num)
         math_sqrt = f'{math_sqrt:.{dec+5}f}'[:-5]
-        
+
         sqrt = user_sqrt(num, dec)
         # print(f'The square root of {num}, up to {dec} decimals is: {sqrt}')
         assert  sqrt == math_sqrt, f'The square root of {num}, up to {dec} decimals is: {sqrt} vs {math_sqrt}'
